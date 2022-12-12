ae6573ef82bc7f67e343dcb1134a002437fa70c4
joao-pedro01 <joao_pedro01@terra.com.br>
2022-12-12 10:16:32 -0300

    Add service

---
+++ src/app/components/user.service.ts
@@ -0,0 +1,12 @@
+import { User } from './user/user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  
+}
